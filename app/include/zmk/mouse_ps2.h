98a7ed3633fcfbb28b2b119d7c8d2a50dc7e5b86
Kim Streich <kim@infusedinsight.com>
2023-04-12 12:39:46 -0600

    feat(mouse): Add PS/2 mouse/trackpoint/etc support

---
+++ app/include/zmk/mouse_ps2.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+int zmk_mouse_ps2_tp_sensitivity_change(int amount);
+int zmk_mouse_ps2_tp_neg_inertia_change(int amount);
+int zmk_mouse_ps2_tp_value6_upper_plateau_speed_change(int amount);
+int zmk_mouse_ps2_tp_pts_threshold_change(int amount);
